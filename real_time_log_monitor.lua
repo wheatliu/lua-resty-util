ad67438986f11698669fd058f307fecd1a11a546
wheat <wheatsliu@gmail.com>
2016-12-01 00:36:37 +0800

    autoformat

--- real_time_log_monitor.lua
+++ real_time_log_monitor.lua
@@ -25,11 +25,9 @@ local wb, err = wsserver.new{
 }
 
 if not wb then
-  ngx.log(ngx.ERR, "init websocket failed: ", err)
-  return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    ngx.log(ngx.ERR, "init websocket failed: ", err)
+    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
-
-
 
 -- global values
 -- inotify init file descriptor
@@ -42,48 +40,51 @@ local file
 local push
 
 local function pushlog(logfile)
-  local err
-  local filename = fs.pathJoin(config.LOGHOME, logfile..'.log')
+    local err
+    local filename = fs.pathJoin(config.LOGHOME, logfile..'.log')
 
-  file = io.open(filename, "r")
-  if not file then
-    ngx.log(ngx.ERR, "NO SUCH FILE:", filename)
-    return ngx.exit(ngx.HTTP_NOT_FOUND)
-  end
-
-  fd = C.inotify_init1(O_NONBLOCK)
-  if fd == -1 then
-    err = C.strerror(ffi.errno())
-    ngx.log(ngx.ERR, 'init inotify failed: ', ffi.string(err))
-    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-  end
-
-  wd = C.inotify_add_watch(fd, filename:cstring(), IN_MODIFY)
-  if wd == -1 then
-    err = C.strerror(ffi.errno())
-    ngx.log(ngx.ERR, 'inotify add watch failed: ', ffi.string(err))
-    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-  end
-
-  local offset = file:seek("end")
-  while true do
-    -- when event occur for monitored file, get the offset and filesize of monitored file
-    -- then to calculate that how many bytes have been appended to monitored file and read it
-    -- send to websocket client
-    ngx.sleep(config.IDENTIFYCHECKINTERVAL)
-    local nread = C.read(fd, buffer,chunk_size);
-    nread = tonumber(nread)
-    if nread >= 0 then
-      local text = file:read("*all")
-      if #text > 0 then
-        local bytes, err = wb:send_text(text)
-      end
-    elseif nread ~= -1 then
-      err = C.strerror(ffi.errno())
-      ngx.log(ngx.ERR, "fetch inotify event error: ", ffi.string(err))
-      return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    file = io.open(filename, "r")
+    if not file then
+        ngx.log(ngx.ERR, "NO SUCH FILE:", filename)
+        return ngx.exit(ngx.HTTP_NOT_FOUND)
     end
-  end
+
+    fd = C.inotify_init1(O_NONBLOCK)
+    if fd == -1 then
+        err = C.strerror(ffi.errno())
+        ngx.log(ngx.ERR, 'init inotify failed: ', ffi.string(err))
+        return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+
+    wd = C.inotify_add_watch(fd, filename:cstring(), IN_MODIFY)
+    if wd == -1 then
+        err = C.strerror(ffi.errno())
+        ngx.log(ngx.ERR, 'inotify add watch failed: ', ffi.string(err))
+        return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+
+    local offset = file:seek("end")
+    while true do
+      -- when event occur for monitored file, get the offset and filesize of monitored file
+      -- then to calculate that how many bytes have been appended to monitored file and read it
+      -- send to websocket client
+      ngx.sleep(config.IDENTIFYCHECKINTERVAL)
+      local nread = C.read(fd, buffer,chunk_size);
+      nread = tonumber(nread)
+      -- rtn code must be 0 or -1 or > 0
+      if nread < -1 then
+          err = C.strerror(ffi.errno())
+          ngx.log(ngx.ERR, "fetch inotify event error: ", ffi.string(err))
+          return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+      end
+
+      if nread > 0 then
+          local text = file:read("*all")
+          if #text > 0 then
+              local bytes, err = wb:send_text(text)
+          end
+      end
+    end
 end
 
 local function sweep()
@@ -116,49 +117,48 @@ local function sweep()
 end
 
 local function keepalive()
-  while true do
-    local data, typ, err = wb:recv_frame()
-    if wb.fatal then
-      cleanjobs()
-      ngx.log(ngx.ERR, "Bad Connection: Receive frame failed.")
-      return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-    end
+    while true do
+        local data, typ, err = wb:recv_frame()
+        if wb.fatal then
+            sweep()
+            ngx.log(ngx.ERR, "Bad Connection: Receive frame failed.")
+            return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+        end
 
-    -- heartbeat
-    -- chrome never send ping frame
-    if not data then
-      local bytes, err = wb:send_ping()
-      if err then
-        ngx.log(ngx.ERR, "SEND PING FAILED: ", err)
+      -- heartbeat
+      -- chrome never send ping frame
+      if not data then
+          local bytes, err = wb:send_ping()
+          if err then
+              ngx.log(ngx.ERR, "SEND PING FAILED: ", err)
+          end
+          ngx.log(ngx.ERR, "Ping frame send")
       end
-      ngx.log(ngx.ERR, "Ping frame send")
-    end
 
-    if typ == "close" then
-      cleanjobs()
-      ngx.log(ngx.ERR, "Connection Closed")
-      break
-    end
+      if typ == "close" then
+          sweep()
+          ngx.log(ngx.ERR, "Connection Closed")
+          break
+      end
 
-    if typ == "ping" then
-      local bytes, err = wb:send_pong()
-      if not bytes then
-        ngx.log(ngx.ERR, "PONG frame send failed: ", err)
-        return ngx.exit(501)
+      if typ == "ping" then
+          local bytes, err = wb:send_pong()
+          if not bytes then
+              ngx.log(ngx.ERR, "PONG frame send failed: ", err)
+              return ngx.exit(501)
+          end
+      end
+
+      if typ == "pong" then
+          ngx.log(ngx.ERR, "PONG frame received")
       end
     end
-
-    if typ == "pong" then
-      ngx.log(ngx.ERR, "PONG frame received")
-    end
-
-  end
 end
 
 local ok, err = ngx.on_abort(sweep)
 if not ok then
-  ngx.log(ngx.ERR, "failed to register the on_abort callback: ", err)
-  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    ngx.log(ngx.ERR, "failed to register the on_abort callback: ", err)
+    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 ngx.log(ngx.ERR, "register the on_abort callback success")
 -- create push task
